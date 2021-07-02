b4aab1f31642bd1b28a8e431e9d9730f96bdfb20
SleepyPlayer16 <63461217+SleepyPlayer16@users.noreply.github.com>
2021-07-02 13:48:56 -0700

    proyecto

---
+++ android/app/src/main/kotlin/com/example/amiibo_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amiibo_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:amiibo_api/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
